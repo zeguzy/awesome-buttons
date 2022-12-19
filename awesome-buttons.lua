1b7e6e671c5b60d9de9495db47f3a4569bd37c1e
vhyc <18573608129@163.com>
2022-12-19 10:37:51 +0800

    [add] 添加几个可以嵌入组件的按钮

--- awesome-buttons.lua
+++ awesome-buttons.lua
@@ -13,7 +13,7 @@ buttons.with_icon = function(args)
     local onclick = args.onclick or function () end
 
     if icon:sub(1, 1) ~= '/' then
-        icon = os.getenv("HOME") .. '/.config/awesome/awesome-buttons/icons/' .. icon .. '.svg'
+        icon = '../icons/' .. icon .. '.svg'
     end
 
     local result = wibox.widget{
@@ -127,6 +127,105 @@ buttons.with_text = function(args)
     return result
 end
 
+buttons.with_time = function(args)
+    local type = args.type or 'basic'
+    local format = args.format or '%Y-%m-%d'
+    local onclick = args.onclick or function() end
+    local color = args.color or '#D8DEE9'
+
+    local result = wibox.widget{
+        {
+            {
+                id = "time",
+                format = format,
+                widget = wibox.widget.textclock,
+            },
+            top = 4, bottom = 4, left = 8, right = 8,
+            widget = wibox.container.margin
+        },
+        bg = '#00000000',
+        shape = function(cr, width, height) gears.shape.rounded_rect(cr, width, height, 4) end,
+        widget = wibox.container.background
+    }
+
+    if type == 'outline' then
+        result:set_shape_border_color(color)
+        result:set_shape_border_width(1)
+    elseif type == 'flat' then
+        result:set_bg(color)
+    end
+
+    local old_cursor, old_wibox
+    result:connect_signal("mouse::enter", function(c)
+        if type ~= 'flat' then
+            c:set_bg("#00000066")
+        end
+        local wb = mouse.current_wibox
+        old_cursor, old_wibox = wb.cursor, wb
+        wb.cursor = "hand1"
+    end)
+    result:connect_signal("mouse::leave", function(c)
+        if type ~= 'flat' then
+            c:set_bg('#00000000')
+        end
+        if old_wibox then
+            old_wibox.cursor = old_cursor
+            old_wibox = nil
+        end
+    end)
+
+    result:connect_signal("button::press", function() onclick() end)
+
+    return result
+end
+
+buttons.with_widget = function(args)
+    local type = args.type or 'basic'
+    local widget = args.widget or {}
+    local onclick = args.onclick or function() end
+    local color = args.color or '#D8DEE9'
+
+    local result = wibox.widget{
+        {
+            widget,
+            top = 4, bottom = 4, left = 8, right = 8,
+            widget = wibox.container.margin
+        },
+        bg = '#00000000',
+        shape = function(cr, width, height) gears.shape.rounded_rect(cr, width, height, 4) end,
+        widget = wibox.container.background
+    }
+
+    if type == 'outline' then
+        result:set_shape_border_color(color)
+        result:set_shape_border_width(1)
+    elseif type == 'flat' then
+        result:set_bg(color)
+    end
+
+    local old_cursor, old_wibox
+    result:connect_signal("mouse::enter", function(c)
+        if type ~= 'flat' then
+            c:set_bg("#00000066")
+        end
+        local wb = mouse.current_wibox
+        old_cursor, old_wibox = wb.cursor, wb
+        wb.cursor = "hand1"
+    end)
+    result:connect_signal("mouse::leave", function(c)
+        if type ~= 'flat' then
+            c:set_bg('#00000000')
+        end
+        if old_wibox then
+            old_wibox.cursor = old_cursor
+            old_wibox = nil
+        end
+    end)
+
+    result:connect_signal("button::press", function() onclick() end)
+
+    return result
+end
 
 buttons.with_icon_and_text = function(args)
 
@@ -160,6 +259,157 @@ buttons.with_icon_and_text = function(args)
                         .. ((type == 'flat') and '#00000000' or color) .. '">' .. text ..'</span>',
                     widget = wibox.widget.textbox
                 },
+                top = 4, bottom = 4, right = 8,
+                widget = wibox.container.margin
+            },
+            layout = wibox.layout.fixed.horizontal
+        },
+        bg = '#00000000',
+        shape = function(cr, width, height) gears.shape.rounded_rect(cr, width, height, 4) end,
+        widget = wibox.container.background
+    }
+
+        if type == 'outline' then
+            result:set_shape_border_color(color)
+            result:set_shape_border_width(1)
+        end
+
+        if type == 'flat' then
+            result:set_bg(color)
+        end
+
+        local old_cursor, old_wibox
+        result:connect_signal("mouse::enter", function(c)
+            if type ~= 'flat' then
+                c:set_bg('#00000044')
+            end
+            pcall(function ()
+                local wb = mouse.current_wibox
+                old_cursor, old_wibox = wb.cursor, wb
+                wb.cursor = "hand1"
+            end)
+        end)
+        result:connect_signal("mouse::leave", function(c)
+            if type ~= 'flat' then
+                c:set_bg('#00000000')
+            end
+            if old_wibox then
+                old_wibox.cursor = old_cursor
+                old_wibox = nil
+            end
+        end)
+
+        result:connect_signal("button::press", function() onclick() end)
+
+        return result
+end
+
+buttons.with_icon_and_date = function(args)
+
+    local type = args.type or 'basic'
+    local icon = args.icon
+    local onclick = args.onclick or function() end
+    local color = args.color or '#D8DEE9'
+    local format = args.format or '%Y-%m-%d'
+
+
+    if icon:sub(1, 1) ~= '/' then
+        icon = os.getenv("HOME") .. '/.config/awesome/awesome-buttons/icons/' .. icon .. '.svg'
+    end
+
+
+    local result = wibox.widget{
+        {
+            {
+                {
+                    image = icon,
+                    resize = true,
+                    forced_height = 20,
+                    widget = wibox.widget.imagebox
+                },
+                margins = 4,
+                widget = wibox.container.margin
+            },
+            {
+                {
+                    id = "time",
+                    format = format,
+                    widget = wibox.widget.textclock,
+                },
+                top = 4, bottom = 4, right = 8,
+                widget = wibox.container.margin
+            },
+            layout = wibox.layout.fixed.horizontal
+        },
+        bg = '#00000000',
+        shape = function(cr, width, height) gears.shape.rounded_rect(cr, width, height, 4) end,
+        widget = wibox.container.background
+    }
+
+        if type == 'outline' then
+            result:set_shape_border_color(color)
+            result:set_shape_border_width(1)
+        end
+
+        if type == 'flat' then
+            result:set_bg(color)
+        end
+
+        local old_cursor, old_wibox
+        result:connect_signal("mouse::enter", function(c)
+            if type ~= 'flat' then
+                c:set_bg('#00000044')
+            end
+            pcall(function ()
+                local wb = mouse.current_wibox
+                old_cursor, old_wibox = wb.cursor, wb
+                wb.cursor = "hand1"
+            end)
+        end)
+        result:connect_signal("mouse::leave", function(c)
+            if type ~= 'flat' then
+                c:set_bg('#00000000')
+            end
+            if old_wibox then
+                old_wibox.cursor = old_cursor
+                old_wibox = nil
+            end
+        end)
+
+        result:connect_signal("button::press", function() onclick() end)
+
+        return result
+end
+
+buttons.with_icon_and_widget = function(args)
+
+    local type = args.type or 'basic'
+    local icon = args.icon
+    local onclick = args.onclick or function() end
+    local color = args.color or '#D8DEE9'
+    local widget = args.widget or {}
+    
+
+
+    if icon:sub(1, 1) ~= '/' then
+        icon = os.getenv("HOME") .. '/.config/awesome/awesome-buttons/icons/' .. icon .. '.svg'
+    end
+
+
+    local result = wibox.widget{
+        {
+            {
+                {
+                    image = icon,
+                    resize = true,
+                    forced_height = 20,
+                    widget = wibox.widget.imagebox
+                },
+                margins = 4,
+                widget = wibox.container.margin
+            },
+            {
+                widget,
                 top = 4, bottom = 4, right = 8,
                 widget = wibox.container.margin
             },
